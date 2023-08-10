7e37c63793ecef7b65cb985009ca1cf9238c19eb
HyerimKimm <50258232+HyerimKimm@users.noreply.github.com>
2023-08-10 16:32:42 +0900

    main Page 반응형 수정

--- client/src/styles/question.js
+++ client/src/styles/question.js
@@ -5,40 +5,34 @@ const globalTokens = tokens.global;
 
 export const Wrapper = styled.li`
 	display: flex;
-	width: 921px;
-	height: 158px;
+	flex-direction: row;
+	align-items: flex-start;
+	width: 90%;
 	padding: 20px 10px;
     margin: 15px;
-	align-items: flex-start;
-	gap: 22px;
-
 	border-radius: 10px;
 	background: #fff;
 	box-shadow: 0px 2px 10px 0px rgba(0, 0, 0, 0.1);
-
 	font-weight: 700;
-
 	color: ${globalTokens.greyColor.value};
 `;
-
 export const Current = styled.aside`
 	display: flex;
-	padding: 10px;
+	width: 10%;
 	flex-direction: column;
 	justify-content: center;
 	align-items: flex-end;
 	gap: 10px;
+	padding-right: 10px;
 `;
 export const Container = styled.article`
+	width: 90%;
 	display: flex;
-	width: 764px;
-	padding: 10px 0px;
 	flex-direction: column;
-	justify-content: space-around;
-	align-items: flex-end;
+	flex-grow: 1;
+	padding: 10px 0px;
 	gap: 10px;
 	flex-shrink: 0;
-
 	h1 {
         font-size: 20px;
 		color: ${globalTokens.blackColor.value};
@@ -46,7 +40,6 @@ export const Container = styled.article`
 `;
 export const Contents = styled.section`
 	display: flex;
-	width: 764px;
 	flex-direction: column;
 	justify-content: center;
 	align-items: flex-start;
@@ -55,16 +48,15 @@ export const Contents = styled.section`
 
 export const Info = styled.div`
 	display: flex;
-	align-items: flex-start;
+	flex-direction: row;
+	justify-content: end;
 	gap: 5px;
 	margin-top: 20px;
+	width: 100%;
 	color: ${globalTokens.lightGreyColor.value};
-
 	section {
-		display: "flex";
 		gap: 5px;
 	}
-
 	span {
 		color: ${globalTokens.blackColor.value};
 	}
