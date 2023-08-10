395e20d0af2501b6e4ef10efbf2c74ede3825095
3kiein <daisy.airdrop_08@icloud.com>
2023-08-10 10:19:57 +0900

    [FEAT] 헤더 버튼 컴포넌트 추가

--- client/src/atoms/Button.js
+++ client/src/atoms/Button.js
@@ -3,53 +3,53 @@ import styled from "styled-components";
 import tokens from "../styles/tokens.json";
 
 export const RegularButtonDesign = styled.button`
-  border-radius: ${tokens.global.regularRadius.value}px;
-  border: 0;
-  font-size: ${tokens.global.smallHeading.value}px;
-  color: ${tokens.global.whiteColor.value};
-  box-shadow: ${tokens.global.regularShadow.value.x}px
-    ${tokens.global.regularShadow.value.y}px
-    ${tokens.global.regularShadow.value.blur}px
-    ${tokens.global.regularShadow.value.spread}px
-    ${tokens.global.regularShadow.value.color};
-  background: ${(props) => props.color || `${tokens.global.mainColor.value}`};
-  width: 350px;
-  height: 70px;
+	border-radius: ${tokens.global.regularRadius.value}px;
+	border: 0;
+	font-size: ${tokens.global.smallHeading.value}px;
+	color: ${tokens.global.whiteColor.value};
+	box-shadow: ${tokens.global.regularShadow.value.x}px
+		${tokens.global.regularShadow.value.y}px
+		${tokens.global.regularShadow.value.blur}px
+		${tokens.global.regularShadow.value.spread}px
+		${tokens.global.regularShadow.value.color};
+	background: ${(props) => props.color || `${tokens.global.mainColor.value}`};
+	width: 350px;
+	height: 70px;
 `;
 
 export const SmallCircleButtonDesign = styled(RegularButtonDesign)`
-  border-radius: 50%;
-  width: 80px;
-  height: 80px;
+	border-radius: 50%;
+	width: 80px;
+	height: 80px;
 `;
 
 export const SmallButtonDesign = styled(RegularButtonDesign)`
-  border-radius: ${tokens.global.regularRadius.value}px;
-  width: 146px;
-  height: 58px;
+	border-radius: ${tokens.global.regularRadius.value}px;
+	width: 146px;
+	height: 58px;
 `;
 
 export const ColorGradBlue = styled.p`
-  background: linear-gradient(to right top, #00e0ff, #006c7a);
-  color: transparent;
-  -webkit-background-clip: text;
-  background-clip: text;
+	background: linear-gradient(to right top, #00e0ff, #006c7a);
+	color: transparent;
+	-webkit-background-clip: text;
+	background-clip: text;
 `;
 
 const Button = ({ color, text }) => {
-  return (
-    <div>
-      <RegularButtonDesign color={color}>
-        <ColorGradBlue>{text}</ColorGradBlue>
-      </RegularButtonDesign>
-      <SmallCircleButtonDesign>
-        <ColorGradBlue>{text}</ColorGradBlue>
-      </SmallCircleButtonDesign>
-      <SmallButtonDesign>
-        <ColorGradBlue>{text}</ColorGradBlue>
-      </SmallButtonDesign>
-    </div>
-  );
+	return (
+		<div>
+			<RegularButtonDesign color={color}>
+				<ColorGradBlue>{text}</ColorGradBlue>
+			</RegularButtonDesign>
+			<SmallCircleButtonDesign>
+				<ColorGradBlue>{text}</ColorGradBlue>
+			</SmallCircleButtonDesign>
+			<SmallButtonDesign>
+				<ColorGradBlue>{text}</ColorGradBlue>
+			</SmallButtonDesign>
+		</div>
+	);
 };
 
 export default Button;
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,12 @@
-import backgroundImg from "../assets/images/headerImage.png";
-import logoImg from "../assets/images/logoImage.png";
 import { Link } from "react-router-dom";
 import { styled } from "styled-components";
+
+import backgroundImg from "../assets/images/headerImage.png";
+import logoImg from "../assets/images/logoImage.png";
+
+import tokens from "../styles/tokens.json";
+import { SmallButtonDesign as Button } from "../atoms/Button";
+import { ColorGradBlue as Gradient } from "../atoms/Button";
 
 const Background = styled.header`
 	background-image: url(${backgroundImg});
@@ -13,7 +18,6 @@ const Background = styled.header`
 	flex-direction: column;
 	align-items: center;
 	justify-content: center;
-	color: white;
 	text-align: center;
 
 	a,
@@ -24,20 +28,55 @@ const Background = styled.header`
 	}
 `;
 
+const Wrapper = styled.div`
+	background-color: rgba(255, 255, 255, 0.5);
+	width: 90%;
+	height: 50%;
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+
+	border-radius: 20px;
+	background: rgba(255, 255, 255, 0.5);
+	position: relative;
+`;
+
 const Title = styled.h1`
 	font-family: "Jolly Lodger";
 	font-size: 32px;
 	color: #0567c2;
 `;
 
+const Buttons = styled.section`
+	position: absolute;
+	right: 0;
+
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
+	padding: 0px 40px;
+`;
+
 export default function Header() {
 	return (
 		<Background>
-			<Link to="/">
-				<img src={logoImg} alt="logo" />
-				<br />
-				<Title>STACK UNDERFLOW</Title>
-			</Link>
+			<Wrapper>
+				<Link to="/">
+					<img src={logoImg} alt="logo" />
+					<br />
+					<Title>STACK UNDERFLOW</Title>
+				</Link>
+				<Buttons>
+					<Link to="/login">
+						<Button color={tokens.global.pointColor.value}>로그인</Button>
+					</Link>
+					<Link to="/signup">
+						<Button color={tokens.global.whiteColor.value}>
+							<Gradient>가입하기</Gradient>
+						</Button>
+					</Link>
+				</Buttons>
+			</Wrapper>
 		</Background>
 	);
 }
