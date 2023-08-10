698ce3b640d55790b36dbd871212c92e9937331f
HelloWorldDogs223 <yonghyeogi377@gmail.com>
2023-08-10 13:06:09 +0900

    Write Page

--- client/src/pages/QnAWritePage.js
+++ client/src/pages/QnAWritePage.js
@@ -1,11 +1,49 @@
-import React from 'react';
+import React from "react";
+import tokens from "../styles/tokens.json";
+import { styled } from "styled-components";
+import Form from "../components/Form";
+
+const QuestionHead = styled.h1`
+  font-size: ${tokens.global.bigHeading.value}px;
+  margin: 10px;
+`;
+
+const QuestionP = styled.p`
+  font-size: ${tokens.global.bodyText.value}px;
+  margin: 10px;
+`;
+
+const TextP = styled(QuestionP)`
+  background-color: ${tokens.global.mainColor.value};
+  margin: 10px;
+  padding: 10px;
+`;
 
 const QnAWritePage = () => {
-    return (
-        <div>
-            This is QnA Write Page.
-        </div>
-    );
+  return (
+    <div>
+      <QuestionHead>질문하세요!</QuestionHead>
+      <div>
+        <TextP>
+          좋은 질문을 작성하는 방법
+          <br /> You’re ready to ask a programming-related question and this
+          form will help guide you through the process. <br />
+          Looking to ask a non-programming question? See the topics here to find
+          a relevant site.
+          <br />
+          Steps <br />
+          Summarize your problem in a one-line title.
+          <br /> Describe your problem in more detail. <br />
+          Describe what you tried and what you expected to happen.
+          <br />
+          Add “tags” which help surface your question to members of the
+          community. <br />
+          Review your question and post it to the site.
+        </TextP>
+      </div>
+      <Form />
+    </div>
+  );
 };
 
 export default QnAWritePage;
